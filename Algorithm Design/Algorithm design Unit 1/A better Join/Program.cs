598bccf34dbc95bfdc63065a666b5eca09bc7eda
MarnixDB01 <marnixdebruijn@gmail.com>
2022-01-14 12:23:07 +0100

    Indie Quest Marnix

---
+++ Algorithm Design/Algorithm design Unit 1/A better Join/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A_better_Join
+{
+    class Program
+    {
+        //Method to decide on when to implement "and" with/without serial comma
+        static string JoinWithAnd(List<string> items, bool useSerialComma = true)
+        {
+            Console.Write($"The heroes in the party are: ");
+
+            //Counting total items in party
+            int count = items.Count;
+            if (count == 0)
+            {
+                return "";
+            }
+            else if (count == 1)
+            {
+                return items[0];
+            }
+            else if (count == 2)
+            {
+                return String.Join(" and ", items);
+            }
+            //If the party is more than 2, then put "and" inbetween last and second last.
+            else
+            {
+                var itemsCopy = new List<string>(items);
+
+                if (useSerialComma)
+                {
+                    itemsCopy[^1] = "and " + itemsCopy[^1];
+                }
+                else 
+                //If no serial comma, then make second to last = second to last + "and" + last. This removes the serial comma.
+                //also removing the last item otherwise it will write twice.
+                {
+                    itemsCopy[^2] = itemsCopy[^2] + " and " + itemsCopy[^1];
+                    itemsCopy.Remove(itemsCopy[^1]);
+                }
+                return string.Join(", ", itemsCopy);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //Party list
+            var party = new List<string> { "Jazlyn", "Theron", "Dayana", "Rolando" };
+
+            //Party with serial comma before the and
+            Console.WriteLine("With serial comma:");
+            Console.WriteLine(JoinWithAnd(party));
+
+            //3 total in party
+            party.Remove("Jazlyn");
+            Console.WriteLine(JoinWithAnd(party));
+
+            //2 total in party
+            party.Remove("Theron");
+            Console.WriteLine(JoinWithAnd(party));
+
+            //1 total in party
+            party.Remove("Dayana");
+            Console.WriteLine(JoinWithAnd(party));
+
+            Console.WriteLine();
+
+            //Restoring the removed party members (in order), otherwise the output will only write "Rolando"
+            party.Remove("Rolando");
+            party.Add("Jazlyn");
+            party.Add("Theron");
+            party.Add("Dayana");
+            party.Add("Rolando");
+
+            //Party without serial comma before the and
+            Console.WriteLine("Without serial comma:");
+            Console.WriteLine(JoinWithAnd(party, false));
+
+            //3 total in party
+            party.Remove("Jazlyn");
+            Console.WriteLine(JoinWithAnd(party, false));
+
+            //2 total in party
+            party.Remove("Theron");
+            Console.WriteLine(JoinWithAnd(party, false));
+
+            //1 total in party
+            party.Remove("Dayana");
+            Console.WriteLine(JoinWithAnd(party, false));
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Algorithm Design/Algorithm design Unit 2/Party dilemma [Algorithm design] - part 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Party_dilemma__Algorithm_design____part_2
+{
+    class Program
+    {
+        static double Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            for (int factorialIndex = 0; factorialIndex < 21; factorialIndex++)
+            {
+                Console.Write($"{factorialIndex}! = ");
+                Console.WriteLine(Factorial(factorialIndex));
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
+
+
