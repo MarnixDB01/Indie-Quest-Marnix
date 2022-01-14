52b10f512c95a42f24d9d3d4e13316e241525868
MarnixDB01 <marnixdebruijn@gmail.com>
2022-01-14 16:18:30 +0100

    Mission post

---
+++ PROJECT 2 Monster Manual/MM Unit 1/Monster Data/Monster Data/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Monster_Data
+{
+    class MonsterEntry
+    {
+        //public strings to save data.
+        public string Name;
+        public string Description;
+        public string Alignment;
+        public string HitPoints;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //list that reads the MonsterManual.txt and stores it inside the list.
+            var monsterEntries = new List<string>(File.ReadAllLines("MonsterManual.txt"));
+
+            var playerEntry = Console.ReadLine();
+            var playerEntryIndex = monsterEntries[monsterEntries.IndexOf(playerEntry)];
+            var playerEntryDescription = playerEntryIndex.Split(',');
+
+            Console.Clear();
+
+            if (monsterEntries.Contains(playerEntry))
+            {
+                monsterEntries.Add(new MonsterEntry
+                {
+                    Name = playerEntry,
+                    Description = playerEntryDescription
+                }); 
+            }
+        }
+    }
+}
