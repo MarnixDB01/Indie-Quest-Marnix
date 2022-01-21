985049930c34f8a8e39565e1ddae424589d1fc19
MarnixDB01 <marnixdebruijn@gmail.com>
2022-01-21 01:31:05 +0100

    MM 2 continuation

--- PROJECT 2 Monster Manual/MM Unit 2/Search by name/Search by name/Program.cs
+++ PROJECT 2 Monster Manual/MM Unit 2/Search by name/Search by name/Program.cs
@@ -16,20 +16,8 @@ namespace Search_by_name
 
     class Program
     {
-        static void Main(string[] args)
+        static void MonsterEntryInformation(string playerEntry, List<string> monsterManualInfo, List<MonsterEntry> monsterEntries)
         {
-            //Reads the MonsterManual.txt and stores it inside the list.
-            var monsterManualInfo = new List<string>(File.ReadAllLines("MonsterManual.txt"));
-
-            //Write out to console asking for player input.
-            Console.WriteLine("Please enter your desired monster name:");
-
-            //Player input.
-            var playerEntry = Console.ReadLine();
-
-            //Clear console so it doesn't stay when writing info to console.
-            Console.Clear();
-
             //Find index of descripion and alignment line from player entry (1 down from playerEntry).
             //Split the line so we can seperate the description and alignment
             string[] playerEntryDescriptionLine = monsterManualInfo[monsterManualInfo.IndexOf(playerEntry) + 1].Split(',');
@@ -47,34 +35,65 @@ namespace Search_by_name
             //Make player Hit Points Roll a seperate string.
             string playerHitPointsRoll = playerEntryHitPointsLine[3];
 
+            //Store current information into a monsterEntries list.
+            monsterEntries.Add(new MonsterEntry
+            {
+                Name = playerEntry,
+                Description = playerEntryDescription,
+                Alignment = playerEntryAlignment,
+                HitPoints = playerHitPoints,
+                HitPointsRoll = playerHitPointsRoll
+            });
+
+            //Write current player entry name to console.
+            Console.WriteLine($"Name: {monsterEntries[0].Name}");
+
+            //Write current player entry description to console.
+            Console.WriteLine($"Description: {monsterEntries[0].Description}");
+
+            //Write current player entry alignment to console.
+            Console.WriteLine($"Alignment:{monsterEntries[0].Alignment}");
+
+            //Write current hit points  player entry to console.
+            Console.WriteLine($"Hit points: {monsterEntries[0].HitPoints} {monsterEntries[0].HitPointsRoll}");
+        }
+
+        static void Main(string[] args)
+        {
+            //Reads the MonsterManual.txt and stores it inside the list.
+            var monsterManualInfo = new List<string>(File.ReadAllLines("MonsterManual.txt"));
+
+            //Tool title.
+            Console.WriteLine("MONSTER MANUAL:");
+            Console.WriteLine();
+
+            //Write out to console asking for player input.
+            Console.WriteLine("Enter a query to search monsters by name:");
+
+            //Player input.
+            var playerEntry = Console.ReadLine();
+
+            //Clear console so it doesn't stay when writing info to console.
+            Console.Clear();
+
             var monsterEntries = new List<MonsterEntry>();
 
             //If playerEntry exists within the manual information
             if (monsterManualInfo.Contains(playerEntry))
             {
-                //Store current information into a monsterEntries list.
-                monsterEntries.Add(new MonsterEntry
-                {
-                    Name = playerEntry,
-                    Description = playerEntryDescription,
-                    Alignment = playerEntryAlignment,
-                    HitPoints = playerHitPoints,
-                    HitPointsRoll = playerHitPointsRoll
-                });
+                Console.WriteLine("Which monster did you want to look up?");
 
-                //Write current player entry name to console.
-                Console.WriteLine($"Name: {monsterEntries[0].Name}");
+                foreach ()
 
-                //Write current player entry description to console.
-                Console.WriteLine($"Description: {monsterEntries[0].Description}");
+                MonsterEntryInformation(playerEntry, monsterManualInfo, monsterEntries);
+            }
+            else
+            {
+                Console.WriteLine("No monsters were found. Try again:");
+                playerEntry = Console.ReadLine();
 
-                //Write current player entry alignment to console.
-                Console.WriteLine($"Alignment:{monsterEntries[0].Alignment}");
-
-                //Write current hit points  player entry to console.
-                Console.WriteLine($"Hit points: {monsterEntries[0].HitPoints} {monsterEntries[0].HitPointsRoll}");
-
-                Console.ReadKey(true);
+                //Clear console so it doesn't stay when writing info to console.
+                Console.Clear();
             }
         }
     }
